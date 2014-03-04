bff0678d0b294bf3941730db07bed3fd2edc4e95
Kevin Swiber <kswiber@gmail.com>
2014-03-04 16:55:29 -0500

    Adding correlation ID to cloud server.

--- cloud_server.js
+++ cloud_server.js
@@ -1,38 +1,62 @@
 var http = require('http');
 var WebSocketServer = require('ws').Server;
 var parseRequest = require('./reqstring');
-var webSocket = null;
 
+var webSocket = null;
+var idCounter = 0;
+
+var clients = {};
 
 var server = http.createServer(function(req, res) {
- if(webSocket) {
-    parseRequest(req, function(err, reqString) {
-      webSocket.send(reqString);
-    });
-    webSocket.once('message', function(data) {
-      var response = data.split('\r\n\r\n');
-      var headersNShit = response.shift().split('\r\n');
-      var body = response.join();
+  if (!webSocket) {
+    res.statusCode = 500;
+    return;
+  };
 
-      var statusLine = headersNShit.shift();
-      headersNShit.forEach(function(header) {
-        var headerPair = header.split(':');
-        res.setHeader(headerPair[0], headerPair[1].trim());
-      });
+  var messageId = ++idCounter;
 
-      statusLine = statusLine.split(' ');
+  clients[messageId] = res;//req.socket; Will need socket for event broadcast.
 
-      res.statusCode = statusLine[1];
-      res.end(body);
+  req.headers['elroy-message-id'] = messageId;
 
-      //req.socket.write(data);
-    });
-  }
-
+  parseRequest(req, function(err, reqString) {
+    webSocket.send(reqString);
+  });
 });
+
+var onmessage = function(data) {
+  var response = data.split('\r\n\r\n');
+  var headersNShit = response.shift().split('\r\n');
+  var body = response.join();
+
+  var statusLine = headersNShit.shift();
+
+  var res;
+
+  headersNShit.forEach(function(header) {
+    var headerPair = header.split(':');
+    if (headerPair[0] === 'elroy-message-id') {
+      res = clients[parseInt(headerPair[1])];
+    }
+  });
+
+  headersNShit.forEach(function(header) {
+    var headerPair = header.split(':');
+    if (headerPair[0] !== 'elroy-message-id') {
+      res.setHeader(headerPair[0], headerPair[1].trim());
+    }
+  });
+
+  statusLine = statusLine.split(' ');
+
+  res.statusCode = statusLine[1];
+  res.end(body);
+};
 
 var wss = new WebSocketServer({ server: server });
 wss.on('connection', function(ws) {
   webSocket = ws;
+  webSocket.on('message', onmessage);
 });
+
 server.listen(process.env.PORT || 3000);
