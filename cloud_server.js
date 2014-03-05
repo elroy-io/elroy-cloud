692cf85edd79d7abaa290570267872cd13922f60
Adam Magaluk <AdamMagaluK@gmail.com>
2014-03-05 15:33:15 -0500

    Added websocket events support.

--- cloud_server.js
+++ cloud_server.js
@@ -1,17 +1,23 @@
 var http = require('http');
 var WebSocketServer = require('ws').Server;
 var parseRequest = require('./reqstring');
+var StompFrame = require('./stomp');
 
 var webSocket = null;
 var idCounter = 0;
 
 var clients = {};
+var subscriptions = {};
 
 var server = http.createServer(function(req, res) {
+
+  console.log('request..')
   if (!webSocket) {
     res.statusCode = 500;
+    res.end();
     return;
   }
+  console.log('request..')
 
   var messageId = ++idCounter;
 
@@ -48,7 +54,11 @@ var onmessage = function(data) {
   });
 
   if(queueName) {
-    console.log('queueName:',queueName,' data:',body);
+    if(subscriptions[queueName]){
+      subscriptions[queueName].forEach(function(client){
+        client.send(JSON.stringify({destination : queueName,data : body}));
+      });
+    }
     return;
   }
 
@@ -67,10 +77,66 @@ var onmessage = function(data) {
   delete clients[messageId];
 };
 
+
+function setupEventSocket(ws){
+  ws.on('message', onEventMessage);
+
+  function closeSocket(){
+    Object.keys(subscriptions).forEach(function(channel){
+      subscriptions[channel].forEach(function(c,idx){
+        if(c === ws)
+          subscriptions[channel].splice(idx,1);  
+      });
+    });
+  }
+
+  ws.on('close',function(){
+    closeSocket();  
+  });
+
+  ws.on('error',function(err){
+    console.log(err);
+    closeSocket();
+  });
+  
+  function onEventMessage (data){
+    var msg = null;
+    try{
+     msg = JSON.parse(data);
+    }catch(err){
+      console.log(err);
+      return;
+    }
+
+    if(msg.cmd === 'subscribe' && msg.name){
+      if(!subscriptions[msg.name])
+        subscriptions[msg.name] = [];
+      subscriptions[msg.name].push(ws);
+
+      var body = 'name='+msg.name;
+
+      var reqStr = 'POST /_subscriptions HTTP/1.1\r\n';
+      reqStr += 'Content-Type:application/x-www-form-urlencoded\r\n';
+      reqStr += 'Host:argo.fog.com\r\n';
+      reqStr += 'Content-Length:'+body.length+'\r\n\r\n';
+      reqStr += body;
+
+      webSocket.send(reqStr);
+
+    }
+  };
+}
+
+
 var wss = new WebSocketServer({ server: server });
 wss.on('connection', function(ws) {
-  webSocket = ws;
-  webSocket.on('message', onmessage);
+  if(ws.upgradeReq.url === '/'){
+    webSocket = ws;
+    webSocket.on('message', onmessage);
+  }else if(ws.upgradeReq.url === '/events'){
+    setupEventSocket(ws);
+  }
 });
+
 
 server.listen(process.env.PORT || 3000);
