902419bd6db5f1756b44a98e755178150df8b41e
Kevin Swiber <kswiber@gmail.com>
2014-03-21 18:56:45 -0400

    Adding collectors to SPDY cloud.

--- cloud.js
+++ cloud.js
@@ -1,133 +1,118 @@
 var http = require('http');
+var spdy = require('spdy');
+var FogAgent = require('./fog_agent');
 var WebSocketServer = require('ws').Server;
-var parseRequest = require('./reqstring');
 
-var ElroyCloud = module.exports = function(){
-  var self = this;
-
-  this._collectors = {};
-
+var ElroyCloud = module.exports = function() {
   this.webSocket = null;
   this.idCounter = 0;
 
+  this._collectors = {};
   this.clients = {};
   this.subscriptions = {};
-  this.idCounter = 0;
 
+  this.agent = null;
+
+  var self = this;
   this.server = http.createServer(function(req, res) {
     if (!self.webSocket) {
       res.statusCode = 500;
       res.end();
       return;
     }
-
     var messageId = ++self.idCounter;
+
     self.clients[messageId] = res;//req.socket; Will need socket for event broadcast.
+
     req.headers['elroy-message-id'] = messageId;
-    parseRequest(req, function(err, reqString) {
-      self.webSocket.send(reqString);
+
+
+    var opts = { method: req.method, headers: req.headers, path: req.url, agent: self.agent };
+    var request = http.request(opts, function(response) {
+      var id = response.headers['elroy-message-id'];
+      var res = self.clients[id];
+
+      Object.keys(response.headers).forEach(function(header) {
+        if (header !== 'elroy-message-id') {
+          res.setHeader(header, response.headers[header]);
+        }
+      });
+
+      response.pipe(res);
+
+      delete self.clients[id];
     });
+
+    req.pipe(request);
   });
 
   this.wss = new WebSocketServer({ server: this.server });
   this.wss.on('connection', function(ws) {
-    if(ws.upgradeReq.url === '/'){
-      self.webSocket = ws;
+    if (ws.upgradeReq.url === '/'){
+      var readable = ws._socket.listeners('readable')[0];
+      ws._socket.removeAllListeners();
+      ws._socket.on('readable', readable);
+
+      self.webSocket = ws._socket;
+
+      self.agent = spdy.createAgent(FogAgent, {
+        host: 'localhost',
+        port: 80,
+        socket: ws._socket,
+        spdy: {
+          plain: true,
+          ssl: false
+        }
+      });
+
+      self.agent.on('push', function(stream) {
+        var data = [];
+        var len = 0;
+        stream.on('readable', function() {
+          while (d = stream.read()) {
+            data.push(d);
+            len += d.length;
+          };
+        });
+
+        stream.on('error', function(err) {
+          console.error('error on push:', err);
+        });
+
+        stream.on('end', function() {
+          var queueName = stream.url;
+          var body = data.join();
+
+          if(self._collectors[queueName]){
+            self._collectors[queueName].forEach(function(collector){
+              collector(data);
+            });
+          }
+
+          if(self.subscriptions[queueName]){
+            self.subscriptions[queueName].forEach(function(client){
+              var data;
+
+              try {
+                data = JSON.parse(body);
+              } catch(e) {
+                data = body;
+              }
+
+              client.send(JSON.stringify({ destination : queueName, data : data }));
+            });
+          }
+        });
+      });
 
       var keys = Object.keys(self._collectors).concat(Object.keys(self.subscriptions));
       keys.forEach(function(k){
-        self._subscribe(k,self.webSocket);  
+        self._subscribe(k);  
       })
-
-      self.webSocket.on('message', self.onmessage.bind(self) );
-    }else if(ws.upgradeReq.url === '/events'){
+    } else if(ws.upgradeReq.url === '/events'){
       self.setupEventSocket(ws);
     }
   });
-}
-
-ElroyCloud.prototype.collector = function(name,collector){
-  if(typeof name === 'function'){
-    collector = name;
-    name = '_logs';
-  }
-
-  if(!this._collectors[name])
-    this._collectors[name] = [];
-
-  this._collectors[name].push(collector);
-
-  return this;
-};
-
-ElroyCloud.prototype.listen = function(){
-  this.server.listen.apply(this.server,arguments);
-  return this;
-};
-
-ElroyCloud.prototype.onmessage = function(data) {
-  var self = this;
-
-  var response = data.split('\r\n\r\n');
-  var headersNShit = response.shift().split('\r\n');
-  var body = response.join();
-
-  var statusLine = headersNShit.shift();
-
-  var res;
-  var messageId;
-  var queueName;
-
-  headersNShit.forEach(function(header) {
-    var headerPair = header.split(':');
-    if (headerPair[0] === 'elroy-message-id') {
-      messageId = headerPair[1];
-      res = self.clients[parseInt(messageId)];
-    }
-
-    if(headerPair[0] === 'elroy-queue-name') {
-      queueName = headerPair[1];
-    }
-  });
-
-
-  if(queueName) {
-    var data;
-    try {
-      data = JSON.parse(body);
-    } catch(e) {
-      data = body;
-    }
-
-    if(self._collectors[queueName]){
-      self._collectors[queueName].forEach(function(collector){
-        collector(data);
-      });
-    }
-
-    if(self.subscriptions[queueName]){
-      var sendData = JSON.stringify({ destination : queueName, data : data });
-      self.subscriptions[queueName].forEach(function(client){
-        client.send(sendData);
-      });
-    }
-
-    return;
-  }
-
-  headersNShit.forEach(function(header) {
-    var headerPair = header.split(':');
-    if (headerPair[0] !== 'elroy-message-id') {
-      res.setHeader(headerPair[0], headerPair[1].trim());
-    }
-  });
-
-  statusLine = statusLine.split(' ');
-
-  res.statusCode = statusLine[1];
-  res.end(body);
-
-  delete self.clients[messageId];
 };
 
 
@@ -166,22 +151,46 @@ ElroyCloud.prototype.setupEventSocket = function(ws){
     if(msg.cmd === 'subscribe' && msg.name){
       if(!self.subscriptions[msg.name])
         self.subscriptions[msg.name] = [];
-      self.subscriptions[msg.name].push(ws);
 
-      if(self.webSocket)
-        self._subscribe(msg.name,self.webSocket);
+      self.subscriptions[msg.name].push(ws);
+      self._subscribe(msg.name);
     }
   };
 }
 
-ElroyCloud.prototype._subscribe = function(event,ws){
+ElroyCloud.prototype.listen = function(){
+  this.server.listen.apply(this.server,arguments);
+  return this;
+};
+
+ElroyCloud.prototype.collector = function(name,collector){
+  if(typeof name === 'function'){
+    collector = name;
+    name = '_logs';
+  }
+
+  if(!this._collectors[name])
+    this._collectors[name] = [];
+
+  this._collectors[name].push(collector);
+
+  return this;
+};
+
+ElroyCloud.prototype._subscribe = function(event) {
   var body = 'name='+event;
 
-  var reqStr = 'POST /_subscriptions HTTP/1.1\r\n';
-  reqStr += 'Content-Type:application/x-www-form-urlencoded\r\n';
-  reqStr += 'Host:argo.fog.com\r\n';
-  reqStr += 'Content-Length:'+body.length+'\r\n\r\n';
-  reqStr += body;
+  var opts = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Host': 'fog.argo.cx',
+      'Content-Length': body.length
+    },
+    path: '/_subscriptions',
+    agent: this.agent
+  };
 
-  ws.send(reqStr);
+  var req = http.request(opts);
+  req.end(new Buffer(body));
 };
